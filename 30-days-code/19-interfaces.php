c6a71fe7ef99ab1efd8fef0184f5af1acdac9ac3
unknown <abhishekyesankar12@gmail.com>
2022-03-29 21:51:13 +0530

    30 days challenge 18

---
+++ 30-days-code/19-interfaces.php
@@ -0,0 +1,20 @@
+<?php
+
+interface AdvancedArithmetic{
+    public function divisorSum($n);
+}
+
+class Calculator implements AdvancedArithmetic {
+    function divisorSum( $n ) {
+        $divisor = [];
+        for( $i = 1; $i <= $n; $i++)  {
+            if( !is_float(  $n / $i ) )  {
+                $divisor[] = $i;
+            }
+        }        
+        return array_sum( $divisor );
+    }
+}
+
+$cal = new Calculator;
+echo $cal->divisorSum( 20 );
